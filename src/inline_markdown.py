1bb03a912e0e04ec4cc46b2d2874294e1839e026
Andy Lai <Andy04Lai@gmail.com>
2024-08-07 20:14:59 +0930

    Spliting on image and link regex added for text nodes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -4,7 +4,9 @@ from textnode import (
     text_type_bold,
     text_type_italic,
     text_type_code,
-    text_type_text
+    text_type_text,
+    text_type_image,
+    text_type_link
 )
 
 # A function that splits text nodes on a delimiter 
@@ -41,3 +43,64 @@ def extract_markdown_images(text):
 # A function that extracts link regex from markdown text and outputs as a tuple (alt text, url)
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != text_type_text:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        # Get image alt text and url
+        images = extract_markdown_images(original_text)
+        # If no images are in the text append the whole node
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+        # Go over each image tuple
+        for image in images:
+            # split the text into 2 with the image link removed
+            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+            # if there are not 2 sections the image link was not correct
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, image section not closed")
+            # Add text if the node is section is not empty
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], text_type_text))
+            # Add the a new text image node into the new nodes
+            new_nodes.append(
+                TextNode(
+                    image[0],
+                    text_type_image,
+                    image[1],
+                )
+            )
+            # Set the text to the next section to check for more images
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, text_type_text))
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != text_type_text:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        links = extract_markdown_links(original_text)
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
+        for link in links:
+            sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, link section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], text_type_text))
+            new_nodes.append(TextNode(link[0], text_type_link, link[1]))
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, text_type_text))
+    return new_nodes
